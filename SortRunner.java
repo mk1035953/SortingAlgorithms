efa3e294e3a00f8b3575e89a2231e89223ae2b06
mk1035953 <mk1035953@wcupa.edu>
2025-09-16 11:28:55 -0400

    Delete SortRunner.java

--- SortRunner.java
+++
@@ -1,8 +0,0 @@
-public class SortRunner {
-    public static void main(String[] args){
-        ShellSort shell = new ShellSort();
-        System.out.println(shell.toString());
-        shell.sort();
-        System.out.println(shell.toString());
-    }
-}
