0e56cd07381540a378f5ee9469eb886257f96a2e
mk1035953 <mk1035953@wcupa.edu>
2025-09-18 09:53:12 -0400

    Update BubbleSort.java

--- Sorts/BubbleSort.java
+++ Sorts/BubbleSort.java
@@ -1,3 +1,4 @@
+package Sorts;
 public class BubbleSort extends Sort{
 
     //Constructors
@@ -37,3 +38,4 @@ public class BubbleSort extends Sort{
         return sumTime/1000000000.0;//Nanoseconds to Seconds
     }
 }
+
